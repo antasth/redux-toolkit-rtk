940a8178de74415cee7de3f9b2e3c73aee33928c
antasth <antasth@mail.ru>
2023-11-14 21:24:47 +0300

    feat: add useActions hook

--- redux/src/components/Recipe/Recipe.jsx
+++ redux/src/components/Recipe/Recipe.jsx
@@ -1,20 +1,20 @@
-import { useDispatch, useSelector } from 'react-redux'
-import { actions } from '../../store/favorites/favorites.slice'
+import { useSelector } from 'react-redux'
+import { useActions } from '../../hooks/UseActions'
 import styles from './Recipe.module.css'
 
 const Recipe = ({ recipe }) => {
-  const { favorites } = useSelector((state) => state)
+  const favorites = useSelector((state) => state.favorites)
 
-  const dispatch = useDispatch()
+  const { toggleFavorites } = useActions()
+
+  const isExists = favorites.some((r) => r.id === recipe.id)
+
   console.log(favorites)
   return (
     <div className={styles.recipe}>
       <h2>{recipe.name}</h2>
-      <button
-        className={styles.button}
-        onClick={() => dispatch(actions.toggleFavorites(recipe))}
-      >
-        Add to favorites
+      <button className={styles.button} onClick={() => toggleFavorites(recipe)}>
+        {isExists ? 'Remove from favorites' : 'Add to favorites'}
       </button>
     </div>
   )
