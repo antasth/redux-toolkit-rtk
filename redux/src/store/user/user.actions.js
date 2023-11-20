f18474a54ae89d9eb93513edabdbb4e26654daab
antasth <antasth@mail.ru>
2023-11-21 00:44:09 +0300

    fix: error in fetchUserById function

--- redux/src/store/user/user.actions.js
+++ redux/src/store/user/user.actions.js
@@ -2,7 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit'
 
 const fetchUserById = (userId) =>
   new Promise((resolve) =>
-    setTimeout(() => resolve({ id: 1, name: 'alex' }), 1000)
+    setTimeout(() => resolve({ id: userId, name: 'alex' }), 1000)
   )
 
 export const getUserById = createAsyncThunk(
